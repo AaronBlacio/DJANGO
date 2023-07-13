e30f7dcec868b7a4374c28a5a5f1a14a5d103cad
AaronBlacio <aaronblacio_2003@outlook.com>
2023-07-12 20:20:06 -0500

    ver

--- Biblioteca/Inventario/urls.py
+++ Biblioteca/Inventario/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.index,name="index"), 
+    path('',views.index,name="index"),
+    path('<int:id>', views.view_producto, name='view_producto'),
 ]
